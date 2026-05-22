57027c66c747397998b83524b9e78992677bd8d9
veraulitinaYa <veraulitina@yandex.ru>
2026-05-22 14:07:48 +0300

    fix: throw exception

--- backend/src/films/films.service.ts
+++ backend/src/films/films.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { Repository } from 'typeorm';
@@ -25,6 +25,10 @@ export class FilmsService {
       relations: ['schedule'],
     });
 
-    return film?.schedule ?? [];
+    if (!film) {
+      throw new NotFoundException('Film not found');
+    }
+
+    return film.schedule;
   }
 }
